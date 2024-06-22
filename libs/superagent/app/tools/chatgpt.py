aca2e9870a843283f49fb25ee97d8d924c5786c3
hocuspocus1212 <hocus.pocus.chat@gmail.com>
2024-06-22 16:37:10 +0500

    Update chatgpt.py

--- libs/superagent/app/tools/chatgpt.py
+++ libs/superagent/app/tools/chatgpt.py
@@ -4,4 +4,5 @@ from langchain_community.tools import AIPluginTool
 
 
 def get_chatpgt_tool(metadata: dict | None, *_args, **_kwargs) -> Any:
+    print("apps>tools>chatgpt.py>get_chatpgt_tool","line 7")
     return AIPluginTool.from_plugin_url(metadata["chatgptPluginURL"])
